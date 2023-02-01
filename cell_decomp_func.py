224ab1b2f9917acd3356276a14baeba112e402cd
Dominic Burrows <45342427+dmnburrows@users.noreply.github.com>
2023-01-31 16:21:26 -0800

    Checkpoint - pyRCTD working on simulated data - BEFORE converting to class

--- cell_decomp_func.py
+++ cell_decomp_func.py
@@ -3,21 +3,20 @@
 def proportions(inp, n_clusts):
 #=============================
     """
-    This function creates a series of empty lists of the same dimension.
-    
+    This function converts a vector of cell type numbers into proportions.
     
     Inputs:
         n_clusts (int): number of cell types
         inp (np array): a gx1 array of cell type numbers, where g is the number of cells in this group 
         
     returns:
-        out_l (list of list): list of list
+        props (np array): a 1xd array of proportions of each cell type, from 0 to d
     
     """
     import numpy as np
 
     props = np.zeros(n_clusts) #create an array of 0s as long as the number of cell types
-    unq = np.unique(inp, return_counts=True) #find unique cell types in this group and theor counts
+    unq = np.unique(inp, return_counts=True) #find unique cell types in this group and their counts
     partial_props = unq[1]/sum(unq[1]) #find the proportions of each cell type
     count=0
     for o in range(len(props)): #loop over each of the cell types and add in proportions in order
