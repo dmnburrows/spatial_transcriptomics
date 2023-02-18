re-running noise parameters with correct calculation of mean rates, and re-writing as functions

cell_decomp_func.py
@@ -1,4 +1,7 @@
 
+import pymc as pm
+import numpy as np
+
 #====================================
 def sample_rates(n_clusts, n_cells, n_genes, rate_range):
 #====================================
@@ -140,10 +143,10 @@ def mean_exp(n_clusts, n_genes, cell_counts, clust_vec):
     """
     import numpy as np
 
-    #Calculate mean gene expression per group
+    #Calculate mean gene expression per group - normalise so that it is a rate of expression as a fraction of all genes
     mean_ref = np.zeros((n_clusts, n_genes))
     for i in range(n_clusts):
-        mean_ref[i] = np.mean(cell_counts[clust_vec == i], axis=0)
+        mean_ref[i] = np.mean(cell_counts[clust_vec == i], axis=0)/np.sum(np.mean(cell_counts[clust_vec == i], axis=0))
 
     return(mean_ref)
 
@@ -213,7 +216,7 @@ class simulate_cell_mix:
     
 
 #==============================================================
-def add_noise(spots, per, a_std, g_std, e_std):
+def add_noise(spots, per, a_std, g_std, e_std, noise_type):
 #==============================================================
 
     """
@@ -225,12 +228,17 @@ def add_noise(spots, per, a_std, g_std, e_std):
         a_std (int): pixel shared gaussian noise - standard deviations 
         g_std (int): gene shared gaussian noise - standard deviations
         e_std (int): independent spot and gene noise - standard deviations
+        noise_type ('string'): type of noise to add
         
     returns:
         spots (np array): y x d array of simulated noisy spot counts, where y = spots, d = genes.
     
     """
     import numpy as np
+
+    if noise_type != 'gaussian' and noise_type != 'exponential':
+        print('noise type must be gaussian or exponential')
+        return()
 
     #Dropout a certain percentage of genes
     if per!=None:
@@ -239,23 +247,125 @@ def add_noise(spots, per, a_std, g_std, e_std):
             spots[i,rand_ind]=0
     
     if e_std!=None:
-        #Add random noise and make int and remove negatives
-        spots = spots+np.random.normal(0, e_std, (spots.shape)) ##EXPONENTIAL OR GAUSSIAN?
+        #Add random noise over each spot and gene
+        if noise_type == 'gaussian': spots = spots+np.random.normal(0, e_std, (spots.shape)) 
+        if noise_type == 'exponential': spots = spots+np.random.exponential(e_std, (spots.shape))
 
     if g_std!=None:
-        #gamma - over each gene
-        gamma = np.random.normal(0, g_std, (spots.shape[1])) ##EXPONENTIAL OR GAUSSIAN?
+        #Add random noise shared across each gene
+        if noise_type == 'gaussian': gamma = np.random.normal(0, g_std, (spots.shape[1]))
+        if noise_type == 'exponential': gamma = np.random.exponential(g_std, (spots.shape[1]))
         gamma_mat = np.asarray([gamma for i in range(spots.shape[0])]) #Repeat across columns for elementwise addition
         spots = spots+gamma_mat
 
     if a_std!=None:
-        #alpha - over each spot
-        alpha = np.random.normal(0, a_std, (spots.shape[0])) ##EXPONENTIAL OR GAUSSIAN?
+        #Add random noise shared across each spot
+        if noise_type == 'gaussian': alpha = np.random.normal(0, a_std, (spots.shape[0]))
+        if noise_type == 'exponential':alpha = np.random.exponential(a_std, (spots.shape[0])) 
         alpha_mat = np.asarray([alpha for i in range(spots.shape[1])]).T #Repeat across columns for elementwise addition
         spots = spots+alpha_mat
 
-    spots = spots.astype(int)  #REMOVE?
+    spots = spots.astype(int)  
     spots[spots < 0] = 0
     spots +=1 #remove any zeros
 
     return(spots)
+
+#==============================================================
+def model_stats(idata, prop_vec):
+#==============================================================
+    mean_post = np.mean(idata.posterior['beta'][0],axis=0)
+
+    # #Calculate relative proportions
+    Nd = np.sum(mean_post, axis=1) 
+    Nd = np.asarray([Nd for i in range(n_clusts)]).T 
+    from scipy.stats import linregress
+    mean_post = np.divide(np.mean(idata.posterior['beta'][0],axis=0),Nd)
+    line_fit=linregress(np.ravel(prop_vec), np.ravel(mean_post))
+    return(mean_post, line_fit.rvalue**2)
+
+
+##########################################################
+##########################################################
+#MY MODELS
+def basic_pymc(n_clusts, n_spots, n_genes, ref_exp):
+    #Simple Linear regression
+    with pm.Model(coords={"celltypes": np.arange(n_clusts),
+                        "spots": np.arange(n_spots),
+                        "genes": np.arange(n_genes) }) as basic_model:
+        #Declare data 
+        mean_exp = pm.Data('mean_exp', ref_exp, mutable=False, dims=['celltypes','genes'])
+        # Priors for unknown model parameters
+        beta=pm.HalfNormal("beta", sigma=1, dims=['spots','celltypes'])
+        lmd= pm.Deterministic('lmd', pm.math.dot(beta, mean_exp), dims=['spots','genes'])
+        #Convert from proportions to counts
+        N_g = pm.Data('N_g', np.sum(spots, 1).reshape(n_spots,1), mutable=False)
+        #Likelihood of observed data given Poisson rates
+        y=pm.Poisson("y", mu=lmd*N_g, observed=spots)
+    return(basic_model)
+
+
+def epsilon_pymc(n_clusts, n_spots, n_genes, ref_exp, noise_type):
+    with pm.Model(coords={"celltypes": np.arange(n_clusts),
+                        "spots": np.arange(n_spots),
+                        "genes": np.arange(n_genes),
+                        "1": np.arange(1) }) as epsilon_model:
+        #Declare data 
+        mean_exp = pm.Data('mean_exp', ref_exp, mutable=False, dims=['celltypes','genes'])
+        # Priors for unknown model parameters
+        beta=pm.HalfNormal("beta", sigma=1, dims=['spots','celltypes']) # celltype proportions
+        if noise_type == 'gaussian': eps=pm.Normal("eps", mu=0, sigma=1, dims=['spots', 'genes'])
+        if noise_type == 'exponential': eps=pm.Exponential("eps", lam=1, dims=['spots', 'genes']) # random noise at each spot and gene
+
+        lmd= pm.Deterministic('lmd', np.exp(eps)*pm.math.dot(beta, mean_exp), dims=['spots','genes'])
+        #Convert from proportions to counts
+        N_g = pm.Data('N_g', np.sum(spots, 1).reshape(n_spots,1), mutable=False)
+
+        #Likelihood of observed data given Poisson rates
+        y=pm.Poisson("y", mu=lmd*N_g, observed=spots)
+    return(epsilon_model)
+
+
+def gamma_pymc(n_clusts, n_spots, n_genes, ref_exp, noise_type):
+    #Poisson noise
+    with pm.Model(coords={"celltypes": np.arange(n_clusts),
+                        "spots": np.arange(n_spots),
+                        "genes": np.arange(n_genes),
+                        "1": np.arange(1) }) as gamma_model:
+        #Declare data 
+        mean_exp = pm.Data('mean_exp', ref_exp, mutable=False, dims=['celltypes','genes'])
+        # Priors for unknown model parameters
+        beta=pm.HalfNormal("beta", sigma=1, dims=['spots','celltypes']) # celltype proportions
+        if noise_type == 'gaussian': gamma = pm.Normal("gamma", mu=0, sigma=1, dims=['1','genes'])
+        if noise_type == 'exponential': gamma = pm.Exponential("gamma", lam=1, dims=['1','genes']) # random noise at each spot and gene
+
+        lmd= pm.Deterministic('lmd', np.exp(gamma)*pm.math.dot(beta, mean_exp), dims=['spots','genes'])
+        #Convert from proportions to counts
+        N_g = pm.Data('N_g', np.sum(spots, 1).reshape(n_spots,1), mutable=False)
+
+        #Likelihood of observed data given Poisson rates
+        y=pm.Poisson("y", mu=lmd*N_g, observed=spots)
+    return(gamma_model)
+
+
+def alpha_pymc(n_clusts, n_spots, n_genes, ref_exp, noise_type):
+    #Poisson noise
+    with pm.Model(coords={"celltypes": np.arange(n_clusts),
+                        "spots": np.arange(n_spots),
+                        "genes": np.arange(n_genes),
+                        "1": np.arange(1) }) as alpha_model:
+        #Declare data 
+        mean_exp = pm.Data('mean_exp', ref_exp, mutable=False, dims=['celltypes','genes'])
+        # Priors for unknown model parameters
+        beta=pm.HalfNormal("beta", sigma=1, dims=['spots','celltypes']) # celltype proportions
+
+        if noise_type == 'gaussian': alpha = pm.Normal("alpha",mu=0, sigma=1, dims=['spots','1'])
+        if noise_type == 'exponential': alpha = pm.Exponential("alpha", lam=1, dims=['spots','1']) # random noise at each spot and gene
+
+        lmd= pm.Deterministic('lmd', np.exp(alpha)*pm.math.dot(beta, mean_exp), dims=['spots','genes'])
+        #Convert from proportions to counts
+        N_g = pm.Data('N_g', np.sum(spots, 1).reshape(n_spots,1), mutable=False)
+
+        #Likelihood of observed data given Poisson rates
+        y=pm.Poisson("y", mu=lmd*N_g, observed=spots)
+    return(alpha_model)