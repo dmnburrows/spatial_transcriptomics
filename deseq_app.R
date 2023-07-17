9c564568b73b7f7306af357e56fbfa3048fec046
dmnburrows <dominicburrows@hotmail.co.uk>
2023-07-17 17:54:58 +0000

    Building dm and counts mat for DESEQ

--- deseq_app.R
+++ deseq_app.R
@@ -1,23 +1,24 @@
 library(DESeq2)
 library(tidyverse)
 
+setwd("/cndd3/dburrows/DATA/spatial_transcriptomics/plaques/DESEQ/plaque_environment/")
 
-
-setwd("/cndd3/dburrows/DATA/spatial_transcriptomics/plaques/DESEQ/")
-
-
-coldata <- read.csv("old-app-cortex-plaque_labels.csv", row.names=1)
-cnts <- read.csv("old-app-cortex-plaque_counts.csv", header=TRUE, check.names=FALSE, row.names=1)
+coldata <- read.csv("old-APP-cortex_plqdist_design.csv", row.names=1)
+cnts <- read.csv("old-APP-cortex_plqdist_counts.csv", header=TRUE, check.names=FALSE, row.names=1)
 
 dds <-DESeqDataSetFromMatrix(countData=cnts, 
                        colData=coldata, 
                        design=~sample+plaque) 
 
 dds <- DESeq(dds)
-res <- results(dds)
-resOrdered <- res[order(res$pvalue),]
-write.csv(as.data.frame(resOrdered), 
+res <- results(dds, alpha=0.1)
+write.csv(as.data.frame(res), 
           file="old-app-cortex-plaque_DESEQ-pval.csv")
+
+
+
+
+
 
 
 
