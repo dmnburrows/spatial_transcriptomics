8080c367d70ba72744dee37d54a9e0394d2b4c87
dmnburrows <dominicburrows@hotmail.co.uk>
2023-07-19 18:51:07 +0000

    MAST analysis

--- mast_app_plqdist.R
+++ mast_app_plqdist.R
@@ -1,25 +1,30 @@
-library()
+library(MAST)
 library(tidyverse)
+library(lme4)
+options(mc.cores=10)
 
 setwd("/cndd3/dburrows/DATA/spatial_transcriptomics/plaques/MAST/plaque_environment/")
 plq_cnts <- read.csv("old-APP-cortex_plqdist_log2p1CPM.csv", header=TRUE, check.names=FALSE, row.names=1)
 plq_mat <- as.matrix(plq_cnts)
 dimnames(plq_mat) <- NULL
 
-plq_cData <- read.csv("old-APP-cortex_plqdist_cData.csv", header=FALSE)
-plq_fData <- read.csv("old-APP-cortex_plqdist_fData.csv", header=FALSE)
-colnames(plq_cData) <- c("wellKey")
+plq_cData <- read.csv("old-APP-cortex_plqdist_design.csv", header=TRUE)
+plq_fData <- read.csv("old-APP-cortex_plqdist_fData-genes.csv", header=FALSE)
 colnames(plq_fData) <- c("primerid")
 
 plq_mast <- FromMatrix(plq_mat, plq_cData, plq_fData)
+plq_filtered<-filterLowExpressedGenes(plq_mast,threshold=0.1)
 
+#random intercept for sample, fixed for dist
+zlmint <- zlm(~dist_nearest_plaq + (1|sample), sca = plq_filtered, method = 'glmer', ebayes = F, strictConvergence=F, fitArgsD = list(nAGQ = 0))
+sum_zlmint <- summary(zlmint, logFC=TRUE, doLRT='dist_nearest_plaq') 
 
+#random intercept and slope for sample, fixed for dist
+zlmint_slope <- zlm(~dist_nearest_plaq + (1+dist_nearest_plaq|sample), sca = plq_filtered, method = 'glmer', ebayes = F, strictConvergence=F, fitArgsD = list(nAGQ = 0))
+sum_zlmint_slope <- summary(zlmint_slope, logFC=TRUE, doLRT='dist_nearest_plaq') 
 
-
-dds <- DESeq(dds)
-res <- results(dds, alpha=0.01,name="dist_nearest_plaq")
-write.csv(as.data.frame(res), 
-          file="old-APP-cortex_plqdist_DESEQ-pval-alpha-01.csv")
+write.csv(sum_zlmint$datatable, file="old-APP-cortex_plqdist_MAST-LRT_sample-intercept.csv")
+write.csv(sum_zlmint_slope$datatable, file="old-APP-cortex_plqdist_MAST-LRT_sample-slopeintercept.csv")
 
 
 
