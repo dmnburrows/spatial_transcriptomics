096163be974f587b13d12189175baa488c9edc94
dmnburrows <dominicburrows@hotmail.co.uk>
2023-07-19 22:05:29 +0000

    ribosomal protein list

--- mast_app_plqdist.R
+++ mast_app_plqdist.R
@@ -2,6 +2,7 @@ library(MAST)
 library(tidyverse)
 library(lme4)
 options(mc.cores=10)
+print('running')
 
 setwd("/cndd3/dburrows/DATA/spatial_transcriptomics/plaques/MAST/plaque_environment/")
 plq_cnts <- read.csv("old-APP-cortex_plqdist_log2p1CPM.csv", header=TRUE, check.names=FALSE, row.names=1)
@@ -18,13 +19,11 @@ plq_filtered<-filterLowExpressedGenes(plq_mast,threshold=0.1)
 #random intercept for sample, fixed for dist
 zlmint <- zlm(~dist_nearest_plaq + (1|sample), sca = plq_filtered, method = 'glmer', ebayes = F, strictConvergence=F, fitArgsD = list(nAGQ = 0))
 sum_zlmint <- summary(zlmint, logFC=TRUE, doLRT='dist_nearest_plaq') 
+write.csv(sum_zlmint$datatable, file="old-APP-cortex_plqdist_MAST-LRT_sample-intercept.csv")
 
 #random intercept and slope for sample, fixed for dist
 zlmint_slope <- zlm(~dist_nearest_plaq + (1+dist_nearest_plaq|sample), sca = plq_filtered, method = 'glmer', ebayes = F, strictConvergence=F, fitArgsD = list(nAGQ = 0))
 sum_zlmint_slope <- summary(zlmint_slope, logFC=TRUE, doLRT='dist_nearest_plaq') 
-
-write.csv(sum_zlmint$datatable, file="old-APP-cortex_plqdist_MAST-LRT_sample-intercept.csv")
 write.csv(sum_zlmint_slope$datatable, file="old-APP-cortex_plqdist_MAST-LRT_sample-slopeintercept.csv")
-
 
 
